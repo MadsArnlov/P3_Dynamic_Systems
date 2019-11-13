e933db5ddb5664975e27393294dd3fb814a3e7cb
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-11-13 14:58:25 +0100

    blablabla

--- vector_fields.py
+++ vector_fields.py
@@ -10,37 +10,20 @@ import numpy as np
 
 # Interval - Axes
 x = np.linspace(-4, 4, 31)
-y = np.linspace(-10, 10, 16)
-
-#z1 = np.linspace(0, 5, 31)
-#z2 = np.linspace(-2.5*np.pi, 2.5*np.pi, 31)
-#z3 = np.linspace(-2, 2, 16)
-#z4 = np.linspace(-2*np.pi, 2*np.pi, 16)
+y = np.linspace(-3, 3, 16)
 
 X, Y = np.meshgrid(x, y)
-#Z1, Z2, Z3, Z4 = np.meshgrid(z1, z2, z3, z4)
-a = 0.0
 
-g, M, m, l_p = 9.82, 10, 7, 1.5
+g = 9.82
+M = 1.1
+m = 0.175
+l_p = 0.282
 
-dy = (g*M)/(m*l_p)*X
+dy = (g*M + g*m)/(M*l_p)*X
 dx = Y
 L = (dx**2 + dy**2)**0.5
 dx = dx/L
 dy = dy/L
-
-#m, M, l, g = 0.5, 2, 1, 9.82
-#
-#dz1 = Z3
-#dz2 = Z4
-#dz3 = (m*g*np.cos(Z2)*np.sin(Z2)-m*l*np.sin(Z2)*Z4**2)/(m+M - m*np.cos(Z2)**2)
-#dz4 = (m*g*np.cos(Z2)**2*np.sin(Z2)-m*l*np.cos(Z2)*np.sin(Z2)*Z4**2)/(m*l+M*l-m*l*np.cos(Z2)**2)+g/l*np.sin(Z2)
-#L = (dz1**2 + dz2**2 + dz3**2 + dz4**2)**0.5
-#dz1 = dz1/L
-#dz2 = dz2/L
-#dz3 = dz3/L
-#dz4 = dz4/L
-
 
 # one step RK4 adapted to system
 def rk4(f, t, x, h):
@@ -52,16 +35,16 @@ def rk4(f, t, x, h):
     return xp, t + h
 
 t_start = 0.0
-t_stop   = 2.0
+t_stop   = 1.0
 N = 500
 t_step = (t_stop - t_start)/float(N)
 
 def fun(t, x):
     return np.array([x[1],
-                      (g*M)/(m*l_p)*x[0]])
+                      (g*(M+m))/(M*l_p)*x[0]])
 
-x10 = -0.52605243180*4
-x20 = 1.6087558040*4
+x10 = 0#-0.52605243180
+x20 = 0#1.6087558040
 
 X1 = np.zeros(N + 1)
 X2 = np.zeros(N + 1)
