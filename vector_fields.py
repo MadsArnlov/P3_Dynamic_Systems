c3be957931892d32c75d105cc95f205ea0c908df
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-11-26 18:07:11 +0100

    changed notation

--- vector_fields.py
+++ vector_fields.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 17 16:19:35 2019
-
-@author: Lars
-"""
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Interval - Axes
-x = np.linspace(-4, 4, 31)
+x = np.linspace(-3, 3, 28)
 y = np.linspace(-3, 3, 16)
 
 X, Y = np.meshgrid(x, y)
@@ -18,12 +12,18 @@ g = 9.82
 M = 1.1
 m = 0.175
 l_p = 0.282
+c = 1
 
-dy = (g*M + g*m)/(M*l_p)*X
-dx = Y
+#dy = (g*M + g*m)/(M*l_p)*X
+#dx = Y
+dy = 2*c*X + Y
+dx = X - 2*c*Y
+dy *= -1
+dx *= -1
 L = (dx**2 + dy**2)**0.5
 dx = dx/L
 dy = dy/L
+
 
 # one step RK4 adapted to system
 def rk4(f, t, x, h):
@@ -34,17 +34,22 @@ def rk4(f, t, x, h):
     xp = x + h*(k1 + 2.0*(k2 + k3) + k4)/6.0
     return xp, t + h
 
+
 t_start = 0.0
-t_stop   = 1.0
+t_stop = 0.5
 N = 500
 t_step = (t_stop - t_start)/float(N)
 
-def fun(t, x):
-    return np.array([x[1],
-                      (g*(M+m))/(M*l_p)*x[0]])
 
-x10 = 0#-0.52605243180
-x20 = 0#1.6087558040
+def fun(t, x):
+    return np.array([x[1], (g*M + g*m)/(M*l_p)*x[0]])
+
+
+A = np.array([[0, 1], [(g*M + g*m)/(M*l_p), 0]])
+eigenvector = np.linalg.eig(A)
+
+x10 = eigenvector[1][0][1]/30
+x20 = eigenvector[1][1][1]/30
 
 X1 = np.zeros(N + 1)
 X2 = np.zeros(N + 1)
@@ -62,7 +67,6 @@ for k in range(N):
 
 plt.figure(figsize=(16, 9))
 plt.quiver(X, Y, dx, dy, color='red', headlength=5)
-#plt.quiver(Z3, Z4, dz3, dz4, color='red', headlength=5)
-plt.plot(X1, X2, 'k-', [x10], [x20], 'kd')
-plt.savefig("direction_field.png")
+#plt.plot(X1, X2, 'k-', [x10], [x20], 'kd')
+#plt.savefig("direction_field.png")
 plt.show()
