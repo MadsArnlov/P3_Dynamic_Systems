204897b9b0c46682489d59ad951b66760cae7cb6
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-12-05 08:15:22 +0100

    experiment

--- vector_fields.py
+++ vector_fields.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Interval - Axes
-x = np.linspace(-3, 3, 28)
-y = np.linspace(-3, 3, 16)
+x = np.linspace(-2, 2, 28)
+y = np.linspace(-30, 30, 16)
 
 X, Y = np.meshgrid(x, y)
 
@@ -14,12 +14,12 @@ m = 0.175
 l_p = 0.282
 c = 1
 
-#dy = (g*M + g*m)/(M*l_p)*X
-#dx = Y
-dy = 2*c*X + Y
-dx = X - 2*c*Y
-dy *= -1
-dx *= -1
+dy = (g*M + g*m)/(M*l_p)*X
+dx = Y
+#dy = 2*c*X + Y
+#dx = X - 2*c*Y
+#dy *= -1
+#dx *= -1
 L = (dx**2 + dy**2)**0.5
 dx = dx/L
 dy = dy/L
@@ -48,8 +48,8 @@ def fun(t, x):
 A = np.array([[0, 1], [(g*M + g*m)/(M*l_p), 0]])
 eigenvector = np.linalg.eig(A)
 
-x10 = eigenvector[1][0][1]/30
-x20 = eigenvector[1][1][1]/30
+x10 = eigenvector[1][0][1]
+x20 = eigenvector[1][1][1]
 
 X1 = np.zeros(N + 1)
 X2 = np.zeros(N + 1)
@@ -67,6 +67,6 @@ for k in range(N):
 
 plt.figure(figsize=(16, 9))
 plt.quiver(X, Y, dx, dy, color='red', headlength=5)
-#plt.plot(X1, X2, 'k-', [x10], [x20], 'kd')
-#plt.savefig("direction_field.png")
+plt.plot(X1, X2, 'k-', [x10], [x20], 'kd')
+plt.savefig("direction_field.png")
 plt.show()
